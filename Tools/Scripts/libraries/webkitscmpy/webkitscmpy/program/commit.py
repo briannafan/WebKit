233405b9b9861319d8e70e417aef0be058263993
Brianna Fan <bfan2@apple.com>
2025-01-30 14:41:20 -0800

    [git-webkit] add option to update list of changed files https://bugs.webkit.org/show_bug.cgi?id=285368 rdar://140809288
    Reviewed by NOBODY (OOPS!).
    
    Add a feature that automatically updates the changelog in your
    commit message with the most recent changes.
    
    This feature will be enabled by default. It can be disabled by:
        1) Set webkitscmpy.auto-update-changelog in .git/config to false
        2) Run git-webkit commit --amend --no-update
    
    * Tools/Scripts/hooks/prepare-commit-msg:
        - When amending a commit with changes, parse the message and update
          the changelog with the modified lines. Then, reconstruct the message.
        - Add the relevant instructions/logging when the changelog is or is not
          updated.
    
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/commit_parser.py:
        - Change regex const names to match the expression (removed <-> deleted)
    (CommitMessageParser._parse_modified_files_lines):
    (CommitMessageParser.apply_comments_to_modified_files_lines):
        - Add return_deleted arg which will only return the lines that end with "Deleted" or "Removed".
        - Catch the case in which a comment is made after "Filename: [Added/Removed/Deleted]."
    (CommitMessageParser.delete_trailing_blank_lines):
        - Fix class method arguments
    
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/commit.py:
    (Commit.parser): Add --update/--no-update flags
    (Commit.main): Set WKSCMPY_UPDATE_CHANGELOG env variable
    
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/setup.py:
    (Setup.git): Set webkitscmpy.auto-update-changelog.
    
    * Tools/Scripts/libraries/webkitscmpy/webkitscmpy/test/setup_unittest.py:

--- Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/commit.py
+++ Tools/Scripts/libraries/webkitscmpy/webkitscmpy/program/commit.py
@@ -26,7 +26,7 @@ import sys
 from .command import Command
 
 from webkitbugspy import Tracker
-from webkitcorepy import run, string_utils
+from webkitcorepy import run, string_utils, arguments
 from webkitscmpy import local
 
 
@@ -57,6 +57,11 @@ class Commit(Command):
             '--amend',
             dest='amend', action='store_true', default=False,
             help='Replace the tip of the current branch by creating a new commit',
+        )
+        parser.add_argument(
+            '--update', '--no-update',
+            dest='update', action=arguments.NoAction, default=None,
+            help='Update the commit message with the most recent changes.'
         )
 
     @classmethod
@@ -98,6 +103,11 @@ class Commit(Command):
         env = os.environ
         env['COMMIT_MESSAGE_TITLE'] = issue.title if issue else ''
         env['COMMIT_MESSAGE_BUG'] = '\n'.join(cls.bug_urls(issue))
+        if args.update:
+            env['WKSCMPY_UPDATE_CHANGELOG'] = 'True'
+        elif args.update is False:
+            env['WKSCMPY_UPDATE_CHANGELOG'] = 'False'
+
         return run(
             [repository.executable(), 'commit', '--date=now'] + additional_args + args.args,
             cwd=repository.root_path,
